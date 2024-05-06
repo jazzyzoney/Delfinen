a170fff0f08d86c3e3522b6c805ffc21cfd96699
Soto2024 <166127027+Soto2024@users.noreply.github.com>
2024-05-06 14:28:48 +0200

    LetsWorkTogether

---
+++ Accounting.java
@@ -0,0 +1,21 @@
+public class Accounting{
+
+//extends member
+
+//Arrayliste<restance>boolean 
+//-KontigentUngdom : int = 1000
+//-KontigentSenior : int = 1600
+//-Kontigent+60 :  int = 1200
+//-KontigentPassive : int = 500
+
+//IsRestance : boolean
+//IsPaid : boolean
+
+//+betalingsFrist
+//+calculateMembershipfee
+//printRestance()
+
+}
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# How To
-Jasmin var her
