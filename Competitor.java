72942afa68472472bd3b67d9d2c51115b3d30fec
jazzyzoney <jaqtash22@gmail.com>
2024-05-08 12:31:50 +0200

    Member class now extends to Competitor

--- Competitor.java
+++ Competitor.java
@@ -1,10 +1,36 @@
 //jasmin
 import java.util.ArrayList;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class Competitor extends Member{
-
-String ;
-int rank;
-
-
-}
+   
+   String coach;
+   String competitionName;
+   int rank;
+   LocalDateTime date;
+   LocalTime result;
+  
+   //getter
+   public String getCoach(){
+      return coach;
+   }
+   public String getCompetitionName(){
+      return competitionName;
+   }
+   public int getRank(){
+      return rank;
+   }
+   public LocalDateTime getDate(){
+      return date;
+   }
+   public LocalTime getResult(){
+      return result;
+   }
+   
+   //setters
+   public void setCoach(String coach){
+    this.coach = coach;
+   }
+   
+   }
