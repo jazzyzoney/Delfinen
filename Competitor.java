68f2015e8b68d46cdcca19e62895af2942a5b531
jazzyzoney <jaqtash22@gmail.com>
2024-05-08 14:32:26 +0200

    competitor

--- Competitor.java
+++ Competitor.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Competitor extends Member{
    
@@ -30,7 +31,22 @@ public class Competitor extends Member{
    
    //setters
    public void setCoach(String coach){
-    this.coach = coach;
+      this.coach = coach;
    }
-   
+   public void setCompetitionName(String competitionName){
+      this.competitionName = competitionName;
    }
+   public void setRank(int rank){
+      this.rank = rank;
+   }
+   public void setDate(String date){
+      // Define a format for the date string (e.g., YYYY-MM-DD)
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");  
+      this.date = LocalDateTime.parse(date, formatter);
+   }
+   public void setResult(String result){
+      // Define a format for the time string (e.g., HH:mm:ss)
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");  
+      this.result = LocalTime.parse(result, formatter);
+   }
+}
