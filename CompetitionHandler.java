f1d7e406fbf33b532b8a0e9c929c137b17cba2b0
jazzyzoney <jaqtash22@gmail.com>
2024-05-11 15:25:41 +0200

    changes

--- CompetitionHandler.java
+++ CompetitionHandler.java
@@ -4,13 +4,15 @@ import java.util.Collections;
 
 public class CompetitionHandler{
 
-//getResult()
-
-   public void RecordResult(){
-   
+   String competitionName;
+   //getter
+   public String getCompetitionName(){
+      return competitionName;
    }
-
-//top 5 swimmers in each swim discipline and age grup 
+   //setter
+   public void setCompetitionName(String competitionName){
+      this.competitionName = competitionName;
+   }
 
 //arraylister junior - 4 discipliner
    public ArrayList<Competitor> juniorSwimmer = new ArrayList<>();
@@ -18,7 +20,6 @@ public class CompetitionHandler{
    public ArrayList<Competitor> juniorCrawl = new ArrayList<>();
    public ArrayList<Competitor> juniorBreast = new ArrayList<>();
    public ArrayList<Competitor> juniorButterfly = new ArrayList<>();
-
 //arraylister senior - 4 discipliner
    public ArrayList<Competitor> seniorSwimmer = new ArrayList<>();
    public ArrayList<Competitor> seniorBackCrawl = new ArrayList<>();
@@ -26,9 +27,62 @@ public class CompetitionHandler{
    public ArrayList<Competitor> seniorBreast = new ArrayList<>();
    public ArrayList<Competitor> seniorButterfly = new ArrayList<>();
 
+   public void addMemberToDiscipline(Competitor competitor){
+      if (competitor.getAge() < 18){
+         for(String discipline : competitor.getDisciplines()){
+            switch (discipline){
+               case "Back Crawl":
+                  juniorBackCrawl.add(competitor);
+                  break;
+               case "Crawl":
+                  juniorCrawl.add(competitor);
+                  break;
+               case "Breast":
+                  juniorBreast.add(competitor);
+                  break;
+               case "Butterfly":
+                  juniorButterfly.add(competitor);
+                  break;
+               default:
+                  System.out.println("invalid");
+            }
+         }
+      } else {
+         for(String discipline : competitor.getDisciplines()){
+            switch (discipline){
+               case "Back Crawl":
+                  seniorBackCrawl.add(competitor);
+                  break;
+               case "Crawl":
+                  seniorCrawl.add(competitor);
+                  break;
+               case "Breast":
+                  seniorBreast.add(competitor);
+                  break;
+               case "Butterfly":
+                  seniorButterfly.add(competitor);
+                  break;
+               default:
+                  System.out.println("invalid");
+            }
+         }
+      }
+   }
 
+//a list with competition results, a list with training results
+   public void RecordResult(){
+      //switch case 1. træning 2. konkurrence
+   }
+   
+   public void GetResults(){
+      
+   }
+
+//top 5 swimmers in each swim discipline and age group
+   public void TopFive(){
+      
+   } 
 }
-
 /* 
 tested code in main:
       CompetitionHandler ch = new CompetitionHandler();
