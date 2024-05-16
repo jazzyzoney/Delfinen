231a99c754d20ffa7fdfc189e7f3f94d4f158236
Soto2024 <166127027+Soto2024@users.noreply.github.com>
2024-05-16 10:42:18 +0200

    Controller

--- Controller.java
+++ Controller.java
@@ -35,16 +35,6 @@ d) Show / print a list of top 5 in every disciplin
 
 
 
-// 1) Search person
-// 2) Create new member
-// 3) Remove member
-// 4) a) Create Invoice b) Update payment  
-// 5) Enter disciplin
-
-// 6) Enter Competition Results
-
-}
-
 //methods
 //Throw /Catch exceptions
 //
