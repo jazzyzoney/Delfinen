09e78b1b3be88be98608154f4747f0d59a871ff7
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-05-21 10:13:45 +0200

    MembersDatabase

---
+++ MembersDatabase.java
@@ -0,0 +1,37 @@
+//Author Caroline B.
+
+import java.util.ArrayList;
+
+public class MembersDatabase {
+
+    private ArrayList<Member> members;
+
+    public MembersDatabase() {
+        members = new ArrayList<>();
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public void printAllMembers() {
+        System.out.println();
+        System.out.println("All Members:");
+
+        for (Member member : members) {
+            System.out.println();
+            System.out.println("Member:");
+            member.print();
+        }
+    
+}
+}
+
+
+
+
+
+
+
+   
+ 
